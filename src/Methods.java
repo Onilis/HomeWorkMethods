bd82f4c4cb2f1896797f0fe6a5e78a90b2ecf003
insai <insaittlg@gmail.com>
2025-01-20 16:23:05 +0300

    Готовая работа.

--- src/Methods.java
+++ src/Methods.java
@@ -1,5 +1,17 @@
 public class Methods {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Задача №1");
+        String firstName = "Иванов";
+        String middleName = "Семён";
+        String lastName = "Семёнович";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
+
+        System.out.println("Задача №2");
+        System.out.println(fullName.toUpperCase());
+
+        System.out.println("Задача №3");
+        String fullName1 = fullName.replace('ё', 'е');
+        System.out.println(fullName1);
     }
 }
